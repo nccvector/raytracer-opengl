5c1fa95e6044dabd8cc31c49c827ca2136d8cf79
nccvector <nccvector@gmail.com>
2019-06-02 13:11:57 +0500

    minor variable adjustments

--- main.glsl
+++ main.glsl
@@ -4,21 +4,31 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-// Params
-const int bounces = 3; // Dont go beyond 20
+// Render params
+const int bounces = 3;
 const int samples = 3;
 const float base_reflection = 0.3;
-float init_energy = 2.0;
-float decay_multiplier = 0.5;
-float roughness = 0.05;
+float energy = 2.0; // Initial light ray energy
+float decay_multiplier = 0.5; // Amount of energy decay per bounce
+float roughness = 0.05; // Light scatter by surface roughness
 
+// Ambient light properties
+float ambient_intensity = 0.05;
+vec3 ambient_color = vec3(1) * ambient_intensity;
+
+// Number of objects in scene (to be used for loops, needs to be global)
+const int num_pointlights = 2;
+const int num_spheres = 3;
+const int num_planes = 6;
+
+// Static naive variables (ignore)
 vec3 EmptyVector = vec3(-69,1000000,-69); // EmptyVector
 float MathInf = 1000000.0; // Math.inf
 
+// Declaring datatypes
 struct Ray{
     vec3 origin;
     vec3 direction;
-    // invert function to be overloaded
 };
 
 struct Plane{
@@ -112,7 +122,7 @@ vec3 hit(Sphere sphere, Ray r){
     return EmptyVector;
 }
 
-// Noise functions
+// Noise function
 float PHI = 1.61803398874989484820459 * 00000.1; // Golden Ratio   
 float PI  = 3.14159265358979323846264 * 00000.1; // PI
 float SQ2 = 1.41421356237309504880169 * 10000.0; // Square Root of Two
@@ -121,26 +131,13 @@ float gold_noise(in vec2 coordinate, in float seed){
     return fract(tan(distance(coordinate*(seed+PHI), vec2(PHI, PI)))*SQ2);
 }
 
-float random (float x) {
-    return fract(sin(x)*43758.5453123);
-}
-// Noise functions end
-
-const int num_pointlights = 2;
-const int num_spheres = 3;
-const int num_planes = 6;
-
-// Ambient Light Intensity
-float ambient_intensity = 0.05;
-vec3 ambient_color = vec3(1) * ambient_intensity;
-float distance_lights = 0.2;
-
+// Trace function
 HitParams trace(Ray r, PointLight pointlights[num_pointlights],
 Plane planes[num_planes], Sphere spheres[num_spheres]){
     // Initializing color with background color
     vec3 color = vec3(1);
 
-    // Initializing closest_hit_point and min_dist with plane
+    // Initializing closest_hit_point
     vec3 closest_hit_point = EmptyVector;
     vec3 obj_normal = EmptyVector;
     float min_dist = MathInf;
@@ -205,8 +202,8 @@ Plane planes[num_planes], Sphere spheres[num_spheres]){
                 }
             }
 
+            // If object_in_way = false check against spheres as well
             if(!object_in_way){
-                // Checking shadow ray against sphere
                 for(int i=0; i<num_spheres; i++){
                     vec3 hit_point = hit(spheres[i], shadow_ray);
                     float dist = distance(shadow_ray.origin, hit_point);
@@ -219,6 +216,7 @@ Plane planes[num_planes], Sphere spheres[num_spheres]){
                 }
             }
 
+            // If still not object in way then shade
             if(!object_in_way){
                 // Shading
                 float diff_angle = dot(shadow_ray.direction, obj_normal);
@@ -234,6 +232,7 @@ Plane planes[num_planes], Sphere spheres[num_spheres]){
 
         }
 
+        // Clipping total light color
         if(total_lightcolor.x > 1.0){
             total_lightcolor.x = 1.0;
         }
@@ -260,7 +259,9 @@ void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
     st.x *= u_resolution.x / u_resolution.y; // maintaining aspect ratio
 
+    
     // Lights definition and properties
+    float distance_lights = 0.2;
     PointLight L;
     L.position = vec3(distance_lights * sin(u_time), 
     0.2, distance_lights * cos(u_time));
@@ -277,6 +278,7 @@ void main() {
     pointlights[0] = L;
     pointlights[1] = L2;
 
+    
     // Spheres definition and properties
     Sphere S;
     S.position = vec3(0.15, 0.0, 0.0);
@@ -297,6 +299,7 @@ void main() {
     spheres[0] = S;
     spheres[1] = S2;
     spheres[2] = S3;
+
 
     // Planes definition and properties
     Plane p; // Floor
@@ -343,7 +346,7 @@ void main() {
     cam.position = vec3(0, 0.1, 0.7);
     vec3 lookAtPosition = vec3(sin(u_time)/10.0,cos(u_time)/10.0,cam.position.z * -2.0);
     cam.forward = normalize(lookAtPosition - cam.position);
-    // cam.forward = vec3(0,0,1);
+    // cam.forward = vec3(0,0,-1); // Use this for static camera
     cam.right = cross(cam.forward, p.normal);
     cam.down = cross(cam.forward, cam.right);
     cam.fov = 80.0;
@@ -354,32 +357,30 @@ void main() {
     mediump float smp = float(samples);
     vec3 total_color;
     for(int s=0; s<samples; s++){
-        // Ray initial cast
-        // Creating ray
+        // Creating initial ray
         Ray initial_ray;
         initial_ray.origin = cam.position;
         mediump float ss = float(s);
         vec3 canvas_position = cam.canvas_origin + (cam.right * (0.5 - st.x+((ss*st.x)/(smp*u_resolution.x)))) + (cam.down * (0.5 - st.y));
         initial_ray.direction = normalize(canvas_position - initial_ray.origin);
-        // Created ray
 
         // Inital hit param to store collision information
         HitParams intial_hit;
-        intial_hit = trace(initial_ray, pointlights, planes, spheres);
+        intial_hit = trace(initial_ray, pointlights, planes, spheres); // trace function implmented above main
 
         // Reflection bounce
         vec3 mul_color = intial_hit.color; // color to multiply
-        float energy = init_energy * decay_multiplier;
-        vec3 total_bounce_color = mul_color * intial_hit.light * energy; // intializing intialially with diffuse color
+        float pass_energy = energy * decay_multiplier; // decaying initial energy and storing it for pass
+        vec3 total_bounce_color = mul_color * intial_hit.light * pass_energy; // intializing with diffuse color
 
-        // intializing previous ray attributes
+        // Intializing previous ray attributes
         vec3 prev_hit_point = intial_hit.position;
         vec3 prev_normal = intial_hit.normal;
         vec3 prev_ray_dir = initial_ray.direction;
 
-        // bounces calculation begin
+        // Calculating bounces
         for(int i=0; i<bounces; i++){
-            Ray r;
+            Ray r; // bounce ray
             r.origin = prev_hit_point + prev_normal * 0.00001;
             r.direction = prev_ray_dir - (prev_normal * (dot(prev_ray_dir, prev_normal) * 2.0));
             r.direction += roughness * normalize(vec3(
@@ -388,23 +389,26 @@ void main() {
                 0.5-gold_noise(st,r.direction.z*10000.0)));
             r.direction = normalize(r.direction);
 
-            HitParams hit;
+            HitParams hit; // bounce ray hit param
             hit = trace(r, pointlights, planes, spheres);
 
-            energy *= decay_multiplier;
-            mul_color *= hit.color; // for glass reflections do mul_color = hit.color
-            mul_color = normalize(mul_color); // normalizing color becuase it will be neutralized by light later
-            total_bounce_color += mul_color * hit.light * energy;
+            pass_energy *= decay_multiplier; // decaying energy after every bounce
+            mul_color *= hit.color; // for glass reflections => mul_color = hit.color
+            mul_color = normalize(mul_color); // normalizing color, it will be neutralized by light
+            total_bounce_color += mul_color * hit.light * pass_energy; // gathering colors of all bounces
 
+            // Updating prev components
             prev_hit_point = hit.position;
             prev_normal = hit.normal;
             prev_ray_dir = r.direction;
         }
 
+        // Applying fresnel
         float fresnel_dot = dot(-initial_ray.direction, intial_hit.normal) - base_reflection;
-        if(fresnel_dot < 0.0){ fresnel_dot = 0.0; }
+        if(fresnel_dot < 0.0){ fresnel_dot = 0.0; } // limiting fresnel amount between 0-1
         total_bounce_color = (intial_hit.color * fresnel_dot) + (total_bounce_color * (1.0 - fresnel_dot));
 
+        // Averaging color gathered over multiple samples
         total_color += total_bounce_color/smp;
     }
 
@@ -413,6 +417,7 @@ void main() {
     if(total_color.y > 1.0){ total_color.y = 1.0; }
     if(total_color.z > 1.0){ total_color.z = 1.0; }
 
+    // Passing final color to render on screen
     gl_FragColor = vec4(total_color, 1);
 
 }
